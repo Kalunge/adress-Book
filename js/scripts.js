73078dcc5717501dc20954245e318524eeb01270
Kalunge <muthomititus+github@gmail.com>
2019-08-04 09:42:41 +0300

    add ADressbook and Contact Contsructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 /* jshint esversion:6 */
 
+//Business LOgic for adress-book ......
+function Adressbook(){
+  this.contacts = [];
+}
+Adressbook.prototype.addContact = function(contact){
+ return this.contacts.push(contact);
+};
+
+let familyAdressBook = new Adressbook();
+let mySister = new Contact('Alice', 'Kaari', '0726436504');
+familyAdressBook.addContact(mySister);
+
+let friendsAdressBook = new Adressbook();
+let eduMutugi = new Contact('Edwin', 'Mutugi', '0718104889');
+friendsAdressBook.addContact(eduMutugi);
+
+//Business Logic for contacrs
 function Contact(firstName, lastName, phoneNUmber){
   this.firstName = firstName;
   this.lastName = lastName;
@@ -9,7 +26,10 @@ function Contact(firstName, lastName, phoneNUmber){
 Contact.prototype.fullName = function(){
   return this.firstName + ' ' + this.lastName;
 };
-let myMother = new Contact('Grace', 'Acibi', 254706236177);
-let faithMuthoni = new Contact('Faith', 'Muthoni', 254713266564);
-let myDad = new Contact('Joseph', 'Kalunge', 254714487730);
+let myMother = new Contact('Grace', 'Acibi', '0706236177');
+let faithMuthoni = new Contact('Faith', 'Muthoni', '0713266564');
+let myDad = new Contact('Joseph', 'Kalunge', '0714487730');
 
+familyAdressBook.addContact(myMother);
+familyAdressBook.addContact(myDad);
+familyAdressBook.addContact(faithMuthoni);
